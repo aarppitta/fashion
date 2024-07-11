64f2e2d60eca5e254ef60699cae3622b44bac1d8
aarppitta <87533332+aarppitta@users.noreply.github.com>
2024-07-11 11:58:50 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(cors({
     credentials: true,
-    origin: ['*'],
+    origin: ['https://fashion-frontend-liard.vercel.app/'],
     methods:['GET','POST'],
 }));
 
